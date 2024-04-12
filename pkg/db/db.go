232eb404e1d7ab1824800675845bff7faf9bda17
Adilfarooque <Adilfarq11@gmail.com>
2024-04-12 14:19:06 +0530

    Added the DB config

---
+++ pkg/db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Adilfarooque/microServices-grpc-project_auth-svc/pkg/models"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Handler struct {
+	DB *gorm.DB
+}
+
+func Init(dbHost, dbPort, dbUser, dbPassword, dbName string) Handler {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(&models.User{})
+
+	return Handler{db}
+}
