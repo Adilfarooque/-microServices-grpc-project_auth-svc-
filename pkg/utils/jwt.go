93c14e3e3d507151255f90c83ad192f8a7115c51
Adilfarooque <Adilfarq11@gmail.com>
2024-04-12 18:12:47 +0530

    Added the Hash and Jwt to manage password and token generate(updated)

--- pkg/utils/jwt.go
+++ pkg/utils/jwt.go
@@ -8,21 +8,21 @@ import (
 	"github.com/golang-jwt/jwt"
 )
 
-type jwtWrapper struct {
+type JwtWrapper struct {
 	SecretKey       string
 	Issuer          string
 	ExpirationHours int64
 }
 
-type jwtClaims struct {
+type JwtClaims struct {
 	jwt.StandardClaims
 	Id    int64
 	Email string
 	Role  string
 }
 
-func (w *jwtWrapper) GenerateToken(user models.User) (signedToken string, err error) {
-	claims := &jwtClaims{
+func (w *JwtWrapper) GenerateToken(user models.User) (signedToken string, err error) {
+	claims := &JwtClaims{
 		Id:    user.Id,
 		Email: user.Email,
 		Role:  "user",
@@ -42,31 +42,51 @@ func (w *jwtWrapper) GenerateToken(user models.User) (signedToken string, err er
 	return signedToken, nil
 }
 
-func (w *jwtWrapper)ValidateToken(SignedToken string)(claims *jwtClaims, err error){
-	token , err := jwt.ParseWithClaims(
+func (w *JwtWrapper) GenerateTokenAdmin(admin models.Admin) (signedToken string, err error) {
+	claims := &JwtClaims{
+		Id:    admin.Id,
+		Email: admin.Email,
+		Role:  "admin",
+
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(w.ExpirationHours)).Unix(),
+			Issuer:    w.Issuer,
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+
+	signedToken, err = token.SignedString([]byte(w.SecretKey))
+
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func (w *JwtWrapper) ValidateToken(SignedToken string) (claims *JwtClaims, err error) {
+	token, err := jwt.ParseWithClaims(
 		SignedToken,
-		&jwtClaims{},
-		func (token *jwt.Token)(interface{} , error)  {
+		&JwtClaims{},
+		func(token *jwt.Token) (interface{}, error) {
 			return []byte(w.SecretKey), nil
 		},
 	)
-	
-	if err != nil{
+
+	if err != nil {
 		return
 	}
 
-	claims , ok := token.Claims.(*jwtClaims)
+	claims, ok := token.Claims.(*JwtClaims)
 
-	
 	if !ok {
-		return nil,errors.New("couldn't parse claims")
+		return nil, errors.New("couldn't parse claims")
 	}
-
 
 	if claims.ExpiresAt < time.Now().Local().Unix() {
 		return nil, errors.New("JWT is expired")
 	}
 
-	
 	return claims, nil
 }
